8a75ff84f4415d0b557390b0cf254653fdd7acf8
Rutkevicius0 <79320802+Rutkevicius0@users.noreply.github.com>
2022-02-10 16:22:53 +0200

    routes for current weather data get and post

--- Routes/routes.js
+++ Routes/routes.js
@@ -16,11 +16,34 @@ router.get('/log/searched-locations', async (req, res) => {
 });
 
 router.post('/log/searched-locations/new', (req, res) => {
+  console.log(`User searched for: ${req.body.keyword}`);
   console.log(req.body);
-
   const newSearchedLocation = new SearchKeywords(req.body);
 
   newSearchedLocation
+    .save()
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
+
+router.get('/log/current-weather', async (req, res) => {
+  try {
+    const result = await CurrentWeather.find();
+    res.json(result);
+  } catch (err) {
+    res.status(500).json('internal error');
+  }
+});
+
+router.post('/log/current-weather/new', (req, res) => {
+  console.log(
+    `Weather condition data at ${req.body.city}, ${req.body.country}  User searched for :`,
+  );
+  console.log(req.body.weatherData);
+
+  const newCurrentWeather = new CurrentWeather(req.body);
+
+  newCurrentWeather
     .save()
     .then((result) => res.json(result))
     .catch((err) => res.json(err));
